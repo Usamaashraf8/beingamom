9e5201a9b85622fc547a75b4660fad8cbec3850c
usama.ashraf8 <usama.ashraf@hhu.de>
2024-09-05 18:17:26 +0200

    updated the manage.py file

--- beingamom/settings.py
+++ beingamom/settings.py
@@ -9,5 +9,7 @@ INSTALLED_APPS = [
     'chat',
 ]
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 LOGIN_REDIRECT_URL = '/chat/'
 LOGOUT_REDIRECT_URL = '/'
+SECRET_KEY = '&6k#t3x&et3$17zuo)%p&o_(9j2q=4=&@3@h-gsqre9uvep_l9'
